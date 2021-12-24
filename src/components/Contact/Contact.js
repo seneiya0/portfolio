6a0cc0d391e5650fe15937a1c6b041f32d19dbb6
seneiya <seneiyacooke@gmail.com>
2021-12-24 09:29:17 -0500

    done

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,6 +7,7 @@ const Contact = () => {
   return (
     <section className='section contact center' id='contact'>
       <h2 className='section__title'>Contact</h2>
+      <h4 style={{marginBottom: '20px', marginTop:'-15px'}}> seneiyacooke@gmail.com </h4>
       <a href={`mailto:${contact.email}`}>
         <span type='button' className='btn btn--outline'>
           Email me
--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,19 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://seneiya0.github.io/',
+  title: 'SC.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Seneiya Cooke',
+  role: 'Full-Stack developer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    'I\'ve completed Flatiron Schools Software Engineering bootcamp and learned JavaScript, React, and Ruby on Rails. I\'ve also started learning python on my own. I love challenging myself and learning new technologies. Aside from coding I love gaming, trying new foods, and spending time with family!',
+  resume: 'https://docs.google.com/document/d/1uSPiN_8vm9j9EiQenbaxoSwoo8NvEk1ZEsScsUOOBbo/edit',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/seneiya/',
+    github: 'https://github.com/seneiya0',
   },
 }
 
@@ -21,28 +21,28 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'Advice Generator',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'This web app generates a random slip of advice upon clicking a button',
+    stack: ['JavaScript','React', 'CSS'],
+    sourceCode: 'https://github.com/seneiya0/advice-generator',
+    livePreview: 'https://advicegen.netlify.app/',
   },
   {
-    name: 'Project 2',
+    name: 'SaidIt',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Forum web app where users can create posts with topics attached to them, as well as upvote/downvote and reply to other users posts.',
+    stack: ['Rails', 'React', 'CSS', 'PostgreSQL'],
+    sourceCode: 'https://github.com/seneiya0/phase-5-capstone-proj',
+    livePreview: 'https://youtu.be/P5oHh399LfM',
   },
   {
-    name: 'Project 3',
+    name: 'Socialize',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Social media web app where users can create an account to post, view, and like other miniblogs.',
+    stack: ['Rails', 'React','CSS', 'PostgreSQL'],
+    sourceCode: 'https://github.com/seneiya0/Phase-4-project',
+    livePreview: 'https://socialize-project-app.herokuapp.com/',
   },
 ]
 
@@ -52,20 +52,20 @@ const skills = [
   'HTML',
   'CSS',
   'JavaScript',
-  'TypeScript',
   'React',
-  'Redux',
-  'SASS',
-  'Material UI',
+  'Ruby',
+  'Rails',
   'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
+  'Ubuntu',
+  'SQLite',
+  'PostgreSQL',
+  'Heroku',
+  'Netlify'
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'seneiyacooke@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
